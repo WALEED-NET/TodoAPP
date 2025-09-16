8454d15c59b6a9b7f4472d7bae6575b0abcf48d3
Waleed Bensumaidea <waleed.com145@gmail.com>
2025-09-16 15:02:38 +0300

    added Core Project

---
+++ TodoAPP.Core/Enum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoAPP.Core
+{
+    public enum Priority
+    {
+        Normal,
+        Medium,
+        High
+
+    }
+}
